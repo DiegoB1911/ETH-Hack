5deeacd79d9083dedbd12c8c29d494ccf48ae0c5
DiegoB1911 <diego.barquero@hotmail.com>
2024-10-25 11:57:47 -0600

    adding new files

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/escrow/:path*', // Proxy requests to this route
+        destination: 'https://api.trustlesswork.com/escrow/:path*', // External API destination
+      },
+    ];
+  },
 };
 
 export default nextConfig;
